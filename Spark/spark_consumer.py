fcfa5b20390c46e54a2bdc4393cc5f831fd9a222
aadithya-naresh <aadithya.naresh5@gmail.com>
2024-05-29 17:47:54 -0400

    Updated Spark Script

--- Spark/spark_consumer.py
+++ Spark/spark_consumer.py
@@ -1,6 +1,7 @@
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType, DoubleType, LongType
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType
 
 # Create a Spark session
 spark = SparkSession.builder \
@@ -29,10 +30,28 @@ df = df.selectExpr("CAST(value AS STRING) as json") \
     .select(from_json(col("json"), schema).alias("data")) \
     .select("data.*")
 
-# Write data to console
+# RDS configuration
+rds_endpoint = os.getenv('RDS_ENDPOINT')
+rds_port = os.getenv('RDS_PORT')
+rds_username = os.getenv('RDS_USERNAME')
+rds_password = os.getenv('RDS_PASSWORD')
+rds_db_name = os.getenv('RDS_DB_NAME')
+
+def write_to_rds(batch_df, batch_id):
+
+    batch_df.write \
+        .format("jdbc") \
+        .option("url", f"jdbc:mysql://{rds_endpoint}/{rds_db_name}") \
+        .option("dbtable", "stock_data") \
+        .option("user", rds_username) \
+        .option("password", rds_password) \
+        .mode("append") \
+        .save()
+        
+# Write data to RDS
 query = df.writeStream \
     .outputMode("append") \
-    .format("console") \
+    .foreachBatch(write_to_rds) \
     .start()
 
 query.awaitTermination()
